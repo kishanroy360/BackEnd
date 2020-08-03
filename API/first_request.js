63ed723b2610580ec6e2e616b8cfce5fa2e1b805
Kishan Roy <kishanroy360@gmail.com>
2020-08-03 11:57:35 +0530

    modified code

--- API/first_request.js
+++ API/first_request.js
@@ -1,1 +1,9 @@
-var request=require("request");
+var request = require('request');
+
+console.log("Sunset in Hawaii is at...");
+request('https://jsonplaceholder.typicode.com/todos', function(error, response, body){
+    if(!error && response.statusCode == 200){
+        var parsedData = JSON.parse(body);
+        console.log(parsedData);
+    }
+});
